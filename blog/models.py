ff98c337c4f0f2387e7f4aac21c82cea94a6817c
Abror Boqiyev <Boqiyevabror@gmail.com>
2025-03-21 10:59:41 +0500

    added Book and Genre to models

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,28 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     age = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=100)     
+
+    def __str__(self):
+        return self.name
+    
+    
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.name
+    
+    
+
     
