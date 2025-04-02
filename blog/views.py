f62f2beeb2644b90c4ff21919c24bd59ef0ecf4b
Abror Boqiyev <Boqiyevabror@gmail.com>
2025-04-02 11:15:33 +0500

    imported Q

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 # from django.shortcuts import render
 from .models import Post
+from django.db.models import Q
 
 
 def blog_list(request):
-    posts = Post.objects.filter(title__endswith='o')
+    posts = Post.objects.filter(Q(author__name__contains="malik")|Q(title__startswith='a'))
     print(posts.query)
     post_list = ""
     for post in posts:
-        post_list += f"<li>{post} / {post.publish}</li>"
+        post_list += f"<li><strong>{post}</strong> | <I>{post.publish}</I>  </br> {post.content}</li>"
     return HttpResponse(f"<ul>{post_list}</ul>")
 
 
